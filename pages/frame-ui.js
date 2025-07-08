b36a5c5bb9ed3a6518e8c955e0918ec6a540b65f
madaXc <47150624+hutaky@users.noreply.github.com>
2025-07-08 14:59:14 +0200

    Update frame-ui.js

--- pages/frame-ui.js
+++ pages/frame-ui.js
@@ -1,4 +1,4 @@
-import FrameContent from "../components/FrameContent";
+import FrameContent from "https://juiced.vercel.app/components/FrameContent";
 
 export default function FrameUI() {
   return <FrameContent />;
