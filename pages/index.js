4f5e9303d71c624b186f30e1a10d92594d2261aa
madaXc <47150624+hutaky@users.noreply.github.com>
2025-07-08 15:06:18 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: 40 }}>
+      <h1>JUICED MiniApp 🍹</h1>
+      <p>This is not a public page. Try <a href="/frame-ui">/frame-ui</a> to test the Frame UI.</p>
+    </div>
+  );
+}
